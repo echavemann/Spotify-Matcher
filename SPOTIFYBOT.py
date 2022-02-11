51281bce2d27a7d8c1f1ce07001ed2d77470e99a
joshuachio <95159047+joshuachio@users.noreply.github.com>
2022-02-11 12:08:31 -0500

    Update SPOTIFYBOT.py
    Final data organization
    - adding sheet per user
    - adding top genres
    - short/medium/long term

--- SPOTIFYBOT.py
+++ SPOTIFYBOT.py
@@ -15,8 +15,6 @@ SCOPE = 'user-top-read', 'user-top-read'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, 
 client_secret=SPOTIPY_CLIENT_SECRET, show_dialog = True, redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE))
 
-top_tracks_short = sp.current_user_top_tracks(limit = 50, offset = 0, time_range="short_term")
-
 short_term_top_tracks = sp.current_user_top_tracks(limit = 50, offset = 0, time_range="short_term")
 medium_term_top_tracks = sp.current_user_top_tracks(limit = 50, offset = 0, time_range="medium_term")
 long_term_top_tracks = sp.current_user_top_tracks(limit = 50, offset = 0, time_range="long_term")
@@ -29,8 +27,9 @@ def get_track_ids(list):
     for song in list['items']:
         track_ids.append(song['id'])
     return track_ids
-
-track_ids = get_track_ids(top_tracks_short)
+short_term_track_ids = get_track_ids(short_term_top_tracks)
+medium_term_track_ids = get_track_ids(medium_term_top_tracks)
+long_term_track_ids = get_track_ids(long_term_top_tracks)
 
 def get_track_features(id):
     meta = sp.track(id)
@@ -38,18 +37,37 @@ def get_track_features(id):
     name = meta['name']
     album = meta['album']['name']
     artist = meta['album']['artists'][0]['name']
-    spotify_url = meta['external_urls']['spotify']
-    album_cover = meta['album']['images'][0]['url']
-    track_info = [name, album, artist, spotify_url, album_cover]
+    track_info = [name, album, artist]
     return track_info
 
-data = []
+header = [["Song Name", "Album", "Artist", '', "Song Name", "Album", "Artist", '', "Song Name", "Album", "Artist", '', "Top Genres"]]
+short_term_data = []
+medium_term_data = []
+long_term_data = []
+genres = []
 
-for track in track_ids:
+for value in genre['genres']:
+    temp = []
+    temp.append(value)
+    genres.append(temp)
+
+for track in short_term_track_ids:
     varList = []
-    for attribute in get_track_features(track)[0:3]:
+    for attribute in get_track_features(track):
         varList.append(attribute)
-    data.append(varList)
+    short_term_data.append(varList)
+
+for track in medium_term_track_ids:
+    varList = []
+    for attribute in get_track_features(track):
+        varList.append(attribute)
+    medium_term_data.append(varList)
+
+for track in long_term_track_ids:
+    varList = []
+    for attribute in get_track_features(track):
+        varList.append(attribute)
+    long_term_data.append(varList)
 
 #Sheets Data
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -62,10 +80,43 @@ spreadsheetURL = '1c1qFAip2v4ih5Rdma5icaVI-pMka7sw-ovbSDd0NAfI'
 
 service = build('sheets', 'v4', credentials=creds)
 
-sheet = service.spreadsheets()
+try:
+    new_sheet = {'requests': [
+                {'addSheet':{'properties':{'title': user_profile_name}}}
+                ]}
 
-new_sheet = {'requests': [{'addSheet':{'properties':{'title': user_profile_name}}}]}
+    res = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetURL, body=new_sheet).execute()
+except:
+    sheet = service.spreadsheets()
+    spreadsheet = service.spreadsheets().get(spreadsheetId=spreadsheetURL).execute()
+    sheet_id = None
+    for _sheet in spreadsheet['sheets']:
+        if _sheet['properties']['title'] == user_profile_name:
+            sheet_id = _sheet['properties']['sheetId']
 
-res = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetURL, body=new_sheet).execute()
+    old_sheet = {'requests': [
+                {'deleteSheet':{'sheetId':sheet_id}}
+                ]}
 
-request = sheet.values().update(spreadsheetId=spreadsheetURL, range='Sheet1!B2', valueInputOption='USER_ENTERED', body={'values':data}).execute()
+    res = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetURL, body=old_sheet).execute()
+    
+    new_sheet = {'requests': [
+                {'addSheet':{'properties':{'title': user_profile_name}}}
+                ]}
+
+    res1 = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetURL, body=new_sheet).execute()
+
+sheet.values().update(spreadsheetId=spreadsheetURL, range=user_profile_name + '!A1', valueInputOption='USER_ENTERED', 
+                                body={'values':header}).execute()
+
+sheet.values().update(spreadsheetId=spreadsheetURL, range=user_profile_name + '!A2', valueInputOption='USER_ENTERED', 
+                                body={'values':short_term_data}).execute()
+
+sheet.values().update(spreadsheetId=spreadsheetURL, range=user_profile_name + '!E2', valueInputOption='USER_ENTERED', 
+                                body={'values':medium_term_data}).execute()
+
+sheet.values().update(spreadsheetId=spreadsheetURL, range=user_profile_name + '!I2', valueInputOption='USER_ENTERED', 
+                                body={'values':long_term_data}).execute()
+
+sheet.values().update(spreadsheetId=spreadsheetURL, range=user_profile_name + '!M2', valueInputOption='USER_ENTERED', 
+                                body={'values':genres}).execute()
